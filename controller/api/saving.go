711882815e8993616bbf540bd61efd12d4b7a265
butaosuinu <buta.osuinu@gmail.com>
2016-11-14 13:34:47 +0900

    add save saving count method

--- controller/api/saving.go
+++ controller/api/saving.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"net/http"
+	"notice_500yen_saving_box/model/saving"
+)
+
+// PostSaving
+// POST /api/v1/saving
+func PostSaving(c echo.Context) (err error) {
+	savingTime := c.FormValue("time")
+	fmt.Println(savingTime)
+	saving.SaveSavingCount(savingTime)
+
+	return c.String(http.StatusOK, "saving")
+}
--- model/saving/savingRepository.go
+++ model/saving/savingRepository.go
@@ -1,1 +1,26 @@
 package saving
+
+import (
+	"fmt"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type SavingData struct {
+	ID   bson.ObjectId `bson:"_id"`
+	Time string        `bson:"time"`
+}
+
+// Saving
+// 貯金結果をDBに保存する
+func SaveSavingCount(time string) {
+	session, _ := mgo.Dial("mongodb://localhost/notice_saving")
+	defer session.Close()
+	db := session.DB("notice_saving")
+	savingCount := &SavingData{
+		Time: time,
+	}
+	col := db.C("saving_count")
+	col.Insert(savingCount)
+	fmt.Println("save success")
+}
